ae05e3a819c6dc4d40a161250c42085f4d6d74d2
Jill Yan <zyan@talend.com>
2017-11-02 13:06:26 +0800

    application time

--- src/main/java/com/qianxun/yoga/YogaController.java
+++ src/main/java/com/qianxun/yoga/YogaController.java
@@ -1,5 +1,8 @@
 package com.qianxun.yoga;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,14 @@ public class YogaController {
 	public String hello(){
 		System.out.println("Hello world!");
 		return "Hello world!";
+	}
+	
+	@RequestMapping(value="/applyOff")
+	public String applyOff(String wechatNickname){
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		String timeStr = sdf.format(new Date());
+		System.out.println("Hello "+wechatNickname+"!");
+		return "Hi "+wechatNickname+"，请假成功 （时间："+timeStr+"）!"; //显示本周请假，本月请假，本学期请假
 	}
 }
